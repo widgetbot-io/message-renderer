087f6fc1750b51e881a7c5de8dae19b42a7a4695
JohnyTheCarrot <tuurmartens4@gmail.com>
2023-06-03 13:53:11 +0200

    fix build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -42,7 +42,7 @@ export default [
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
+    input: "dist/cjs/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
