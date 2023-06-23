e99b55c32bd35a14394d04570383f107b774d2e2
JohnyTheCarrot <tuurmartens4@gmail.com>
2023-06-23 17:12:31 +0200

    fix missing svg not loading & switch to commonjs build for now

--- src/core/svgs.ts
+++ src/core/svgs.ts
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 
-import SvgMissingAsset from "../assets/libAssets/missing-asset.svg";
+const SvgMissingAsset =
+  "data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' fill='none' xmlns='http://www.w3.org/2000/svg' id='svg'%3E%3Cg clip-path='url(%23clip0_604_44)'%3E%3Crect width='64' height='64' rx='6' fill='black'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M0 0H32V32H0V0ZM64 32V64H32V32H64Z' fill='%23DC3ADF'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_604_44'%3E%3Crect width='64' height='64' rx='6' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A";
 import { ConfigContext } from "./ConfigContext";
 
 export type SvgConfig = Record<Svg, string>;
