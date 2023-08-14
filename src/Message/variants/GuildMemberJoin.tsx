21616608b46507ec42fde003f06dbda4a1d7dbc3
JohnyTheCarrot <tuurmartens4@gmail.com>
2023-08-14 16:05:55 +0200

    make requested changes and fix bug

--- src/Message/variants/GuildMemberJoin.tsx
+++ src/Message/variants/GuildMemberJoin.tsx
@@ -3,9 +3,10 @@ import LargeTimestamp from "../LargeTimestamp";
 import React from "react";
 import * as Styles from "../style/message";
 import { SystemMessageIconSize } from "../style/message";
-import type { APIMessage, Snowflake } from "discord-api-types/v10";
+import type { APIMessage } from "discord-api-types/v10";
 import { useConfig } from "../../core/ConfigContext";
 import { Trans, useTranslation } from "react-i18next";
+import { GuildMemberJoin as GuildMemberJoinI18n } from "../../i18n/locales/en/translation.json";
 
 interface GuildMemberJoinProps {
   createdAt: APIMessage["timestamp"];
@@ -13,106 +14,19 @@ interface GuildMemberJoinProps {
   channelId: APIMessage["channel_id"];
 }
 
-function joinMessage(
-  createdAt: APIMessage["timestamp"],
-  author: APIMessage["author"],
-  guildId: Snowflake | undefined | null
-): JSX.Element {
-  function Member() {
-    return <MessageAuthor author={author} guildId={guildId} onlyShowUsername />;
-  }
-
-  const { t } = useTranslation();
-
-  const messages = [
-    <Trans
-      i18nKey="joinMessages.0"
-      key="GuildMemberJoin.joinMessage.0"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.1"
-      key="GuildMemberJoin.joinMessage.1"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.2"
-      key="GuildMemberJoin.joinMessage.2"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.3"
-      key="GuildMemberJoin.joinMessage.3"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.4"
-      key="GuildMemberJoin.joinMessage.4"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.5"
-      key="GuildMemberJoin.joinMessage.5"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.6"
-      key="GuildMemberJoin.joinMessage.6"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.7"
-      key="GuildMemberJoin.joinMessage.7"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.8"
-      key="GuildMemberJoin.joinMessage.8"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.9"
-      key="GuildMemberJoin.joinMessage.9"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.10"
-      key="GuildMemberJoin.joinMessage.10"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.11"
-      key="GuildMemberJoin.joinMessage.11"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-    <Trans
-      i18nKey="joinMessages.12"
-      key="GuildMemberJoin.joinMessage.12"
-      t={t}
-      components={{ Member: <Member /> }}
-    />,
-  ];
-
-  return messages[Number(new Date(createdAt)) % messages.length];
-}
-
 function GuildMemberJoin(props: GuildMemberJoinProps) {
   const { resolveChannel } = useConfig();
   const channel = resolveChannel(props.channelId);
   const guildId =
     channel !== null && "guild_id" in channel ? channel.guild_id : null;
+
+  const { t } = useTranslation();
+
+  const numberOfJoinMessages = Object.keys(
+    GuildMemberJoinI18n.joinMessage
+  ).length;
+  const joinMessageIndex =
+    Number(new Date(props.createdAt)) % numberOfJoinMessages;
 
   return (
     <Styles.SystemMessage>
@@ -122,7 +36,19 @@ function GuildMemberJoin(props: GuildMemberJoinProps) {
         svg="IconAdd"
       />
       <Styles.SystemMessageContent>
-        {joinMessage(props.createdAt, props.author, guildId)}
+        <Trans
+          i18nKey={`GuildMemberJoin.joinMessage.${joinMessageIndex}`}
+          t={t}
+          components={{
+            Member: (
+              <MessageAuthor
+                author={props.author}
+                guildId={guildId}
+                onlyShowUsername
+              />
+            ),
+          }}
+        />
         <LargeTimestamp timestamp={props.createdAt} />
       </Styles.SystemMessageContent>
     </Styles.SystemMessage>
